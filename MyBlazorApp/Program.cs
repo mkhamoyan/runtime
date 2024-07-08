6e4fb0a0c1749b91afea0f51c604458d3d925836
Meri Khamoyan <merikhamoyan@microsoft.com>
2024-07-08 14:19:22 +0200

    create blazor app

---
+++ MyBlazorApp/Program.cs
@@ -0,0 +1,14 @@
+#pragma warning disable IDE0073
+
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MyBlazorApp;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync().ConfigureAwait(false);
+#pragma warning restore IDE0073
